e9d8af05daf7cd425678c7e1e21a9aa8963ee1a0
KapilPatil07 <53969423+KapilPatil07@users.noreply.github.com>
2019-10-20 17:45:40 +0530

    update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if(isset($_SESSION["uid"])){
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Ecommerce</title>
+		<title>Ecommerce Portal</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css"/>
 		<script src="js/jquery2.js"></script>
 		<script src="js/bootstrap.min.js"></script>
